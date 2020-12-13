2ccc134a95a6bd115c659568d999d6a7df026a4f
Vineet Badrinath <vbadrina@redhat.com>
2020-12-13 23:45:47 +0530

    Adds quickstart files to container during build
    What is a QuickStart ?
    Quick starts walk users through completing different tasks in the console. In OpenShift 4.7, we added a quick start custom resource. This allows operators and administrators to contribute new quick starts to the cluster beyond the out-of-the-box set. Typically, quick starts for operators are created by the operator itself after the operator is installed.
    
    What does this PR add ?
    This commit adds ensureQuickStarts and relevant dependencies to initialize the new QuickStarts (OCS-Configuration, Getting Started with OCS).
    
    Links
    README: https://github.com/openshift/console-operator/tree/master/quickstarts
    GUIDELINES: http://openshift.github.io/openshift-origin-design/conventions/documentation/quick-starts.html
    
    Signed-off-by: Vineet Badrinath <vbadrina@redhat.com>

--- hack/build-container.sh
+++ hack/build-container.sh
@@ -5,4 +5,5 @@ set -e
 source hack/common.sh
 
 cp $PROMETHEUS_RULES_PATH/*rules*.yaml $OUTDIR
+cp $QUICKSTARTS_PATH/*quickstart*.yaml $OUTDIR
 ${IMAGE_BUILD_CMD} build -f build/Dockerfile.build -t "${OPERATOR_FULL_IMAGE_NAME}" .
